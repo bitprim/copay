461f1784a85634e4c55b86da3fbe0b8802943a2c
Dario Ramos <dario.fenix@gmail.com>
2018-12-21 11:33:34 -0300

    [KEOK-168] Get real assets list from backend

--- src/pages/add/add-asset/add-asset.ts
+++ src/pages/add/add-asset/add-asset.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
-import { StatusBar } from '@ionic-native/status-bar';
-import { Platform } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
+import { Logger } from '../../../providers/logger/logger';
 
 @Component({
   selector: 'add-asset',
   templateUrl: 'add-asset.html'
 })
 export class AddAssetPage {
-  constructor(private platform: Platform, private statusBar: StatusBar) {}
+  public assets;
+  private wallet;
 
-  ionViewWillEnter() {
-    if (this.platform.is('ios')) {
-      this.statusBar.styleDefault();
-    }
-  }
-
-  ionViewWillLeave() {
-    if (this.platform.is('ios')) {
-      this.statusBar.styleLightContent();
-    }
+  constructor(public logger: Logger, private navParams: NavParams) {
+    this.wallet = this.navParams.data.wallet;
+    this.wallet.getKeokenAssets(
+      function(err, assets) {
+        if (err) {
+          // log error
+          return;
+        }
+        this.assets = assets;
+        this.logger.info(JSON.stringify(assets));
+      }.bind(this)
+    );
   }
 }
